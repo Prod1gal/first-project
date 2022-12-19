6143c44fee671c7415099be73023f66bf6258b96
Prod1gal <ar5.j@yandex.ru>
2022-12-19 13:23:24 +0300

    Добавлен main() в Praktikum

--- Praktikum.java
+++ Praktikum.java
@@ -3,6 +3,6 @@ import java.util.Random;
 class Praktikum {
     public static void main(String[] args) {
         
-        System.out.println("Первый коммит";
+        System.out.println("Первый коммит и класс main";
     }
 }
